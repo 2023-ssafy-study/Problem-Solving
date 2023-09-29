eeb958734d058edac7d2de41143a64c80240e160
zzz0105 <lisajieun@naver.com>
2023-09-29 16:33:19 +0900

    Revert "solve: 20207, 11052 문제 풀이 완료"
    This reverts commit dc3f6e04df5af53fbbe32db81ac31eb3fe640a51.

--- jieun/BOJ/11052. 카드 구매하기.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-p = tuple(map(int, input().split()))
-dp = [0] * (N + 1)
-
-for i in range(N + 1):
-    for j in range(i):
-        dp[i] = max(dp[i - j - 1] + p[j], dp[i])
-
-print(dp[N])
