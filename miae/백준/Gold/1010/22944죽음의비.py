919d02d67a85a48c74c3c4d43867418fe4219c74
aldoremiae-e <miaemoo83@gmail.com>
2023-10-11 20:44:02 +0900

    feat:modify 죽음의비

--- miae/백준/Gold/1010/22944죽음의비.py
+++ miae/백준/Gold/1010/22944죽음의비.py
@@ -16,28 +16,32 @@ dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 def bfs():
     global visit
     q = deque()
-    q.append((si, sj, H, 0, 0))
-    visit[si][sj] = H
+    q.append((si, sj, H, 0))
+    visit[si][sj] = 1
     while q:
-        ci, cj, ch, cd, cnt = q.popleft()
-        nh, nd = ch, cd
+        ci, cj, ch, cd = q.popleft()
         if ch == 0:
             continue
         for di, dj in dirs:
             ni, nj = ci + di, cj + dj
             if not (0 <= ni < N and 0 <= nj < N):
                 continue
-            if ni == ei and nj == ej:
-                return cnt + 1
+            if visit[ni][nj] > 0:
+                continue
             if board[ni][nj] == 'U':
-                nd = D-1
+                visit[ni][nj] = visit[ci][cj] + 1
+                q.append((ni, nj, ch, D-1))
             elif board[ni][nj] == '.':
+                visit[ni][nj] = visit[ci][cj] + 1
+                nh, nd = ch, cd
                 if cd > 0:
                     nd -= 1
                 else:
                     nh -= 1
-            if visit[ni][nj] < nh:
-                visit[ni][nj] = nh
-                q.append((ni, nj, nh, nd, cnt+1))
-    return -1
-print(bfs())
+                q.append((ni, nj, nh, nd))
+            else:
+                visit[ni][nj] = visit[ci][cj] + 1
+                return
+
+bfs()
+print(visit[ei][ej] - 1)
